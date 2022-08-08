554baaa402d6d42bb53d13237180a13b41f4aaa4
nir-limor-coralogix <104307879+nir-limor-coralogix@users.noreply.github.com>
2022-08-08 15:37:31 +0300

    Update web-server.sh

--- ec2-bootstrap/web-server.sh
+++ ec2-bootstrap/web-server.sh
@@ -12,4 +12,6 @@ echo '</h3> <h3>Public IP: ' >> /var/www/html/index.html
 curl http://169.254.169.254/latest/meta-data/public-ipv4 >> /var/www/html/index.html
 echo '</h3> <h3>Local IP: ' >> /var/www/html/index.html
 curl http://169.254.169.254/latest/meta-data/local-ipv4 >> /var/www/html/index.html
+echo '</h3> <h3>Local Timestamp: ' >> /var/www/html/index.html
+date >> /var/www/html/index.html
 echo '</h3></html> ' >> /var/www/html/index.html
