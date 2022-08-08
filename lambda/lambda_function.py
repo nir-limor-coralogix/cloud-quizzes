7105e0849198b420446fc17112323863cf315126
nir-limor-coralogix <104307879+nir-limor-coralogix@users.noreply.github.com>
2022-08-08 22:16:29 +0300

    Update lambda_function.py

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,8 +1,6 @@
 def lambda_handler(event, context):
     message = 'Hello {} {}! Keep being awesome!'.format(event['first_name'], event['last_name'])  
-
     print(message)
-
     return { 
         'message' : message
     } 
