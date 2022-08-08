2e7211836d69559246cc59a002e9123f2a2ea39b
nir-limor-coralogix <104307879+nir-limor-coralogix@users.noreply.github.com>
2022-08-08 16:58:26 +0300

    Update web-server.sh

--- ec2-bootstrap/web-server.sh
+++ ec2-bootstrap/web-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update -y
-sudo apt-get install apache2 unzip -y
+sudo apt-get install apache2 unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
