c259b035e35f6acbbbec09672c19576b4c1ae3a6
nir-limor-coralogix <104307879+nir-limor-coralogix@users.noreply.github.com>
2022-08-08 22:16:10 +0300

    Update lambda_function.py

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,7 +1,6 @@
 def lambda_handler(event, context):
     message = 'Hello {} {}! Keep being awesome!'.format(event['first_name'], event['last_name'])  
 
-    #print to CloudWatch logs
     print(message)
 
     return { 
