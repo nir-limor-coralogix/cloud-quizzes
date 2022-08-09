25e344fb31f2a75038a8f4e41654dc01481c3f80
nir-limor-coralogix <104307879+nir-limor-coralogix@users.noreply.github.com>
2022-08-09 18:30:43 +0300

    Update web-server.sh

--- ec2-bootstrap/web-server.sh
+++ ec2-bootstrap/web-server.sh
@@ -4,7 +4,7 @@ sudo apt-get install apache2 unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-echo '<html><h1>Bootstrap Demo</h1><h3>Availability Zone: ' > /var/www/html/index.html
+echo '<html><h1>Snowbit Demo</h1><h3>Availability Zone: ' > /var/www/html/index.html
 curl http://169.254.169.254/latest/meta-data/placement/availability-zone >> /var/www/html/index.html
 echo '</h3> <h3>Instance Id: ' >> /var/www/html/index.html
 curl http://169.254.169.254/latest/meta-data/instance-id >> /var/www/html/index.html
